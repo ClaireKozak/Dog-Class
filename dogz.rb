d77c6fad529e601ccb5d3df72d5f66bd4a65313a
ClaireKozak <crk.pluto@gmail.com>
2014-06-23 13:53:16 -0400

    create Dog Class

---
+++ dogz.rb
@@ -0,0 +1,35 @@
+class Dog
+
+	def initialize(name, breed, leg_count)
+		@name = name
+		@breed = breed
+		@leg count = leg_count
+		#dog needs to bark
+		
+	end
+
+	def name
+		@name
+	end
+
+	def name=(name)
+		@name=name
+	end
+
+	def breed
+		@breed
+	end
+
+	def breed=(breed)
+		@breed=breed
+	end
+
+	def leg_count
+		@leg_count
+	end
+
+	def leg_count=(leg_count)
+		@leg_count=leg_count
+	end
+
+end
